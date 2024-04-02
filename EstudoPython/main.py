951a0d0a93e3a3f833952fa545d21996fb245eed
Gabriel Lopes <carnagesagas@gmail.com>
2024-04-02 09:01:36 -0300

    criado a classe Conta

--- EstudoPython/main.py
+++ EstudoPython/main.py
@@ -1,1 +1,13 @@
-print("Testando codigo")
+class Main:
+    pass
+
+print("testando o codigo")
+
+from Cliente import Cliente
+
+from Conta import Conta
+
+c1= Cliente("João", "12345-6789")
+conta= Conta(c1.nome, 6565, 0)
+
+print(conta.titular, " Numero: ", conta.numero, "Seu saldo: ", conta.saldo)
