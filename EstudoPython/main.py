4c3c5d232cce1c7c6ee41d3a600678dc5753f840
Gabriel Lopes <carnagesagas@gmail.com>
2024-04-02 14:48:19 -0300

    adicionado metodos a classe conta

--- EstudoPython/main.py
+++ EstudoPython/main.py
@@ -8,6 +8,9 @@ from Cliente import Cliente
 from Conta import Conta
 
 c1= Cliente("João", "12345-6789")
-conta= Conta(c1.nome, 6565, 0)
+conta= Conta(c1.get_nome(), 1222)
 
-print(conta.titular, " Numero: ", conta.numero, "Seu saldo: ", conta.saldo)
+conta.deposita(100)
+conta.saque(50)
+conta.extrato()
+
