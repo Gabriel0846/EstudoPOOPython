60476145bebbc90bdd31923a3b5b339fcc14f7eb
Gabriel Lopes <carnagesagas@gmail.com>
2024-04-04 08:19:26 -0300

    teste de botão com resposta

--- EstudoPython/Python_UI/MainApp.py
+++ EstudoPython/Python_UI/MainApp.py
@@ -1,14 +1,17 @@
 from kivy.app import App
-from kivy.uix.image import  Image, AsyncImage
+from kivy.uix.button import Button
 
 class MainApp(App):
     def build(self):
-        img = AsyncImage(source = 'https://supermariorun.com/assets.img/stare/mario03.png',
-            size_hint = (1, .5),
-            pos_hint = {'center_x':.5, 'center_y':.5})
-        return img
+        button = Button(text = 'Fala galera!',
+            size_hint = (.5, .5),
+            pos_hint = {'center_x': .5, 'center_y': .5})
+        button.bind(on_press = self.on_press_button)
+        return button
 
-if __name__ ==  '__main__':
+    def on_press_button(selfself, instance):
+        print('Você apertou o botão!')
+
+if __name__ == '__main__':
     app = MainApp()
     app.run()
-
