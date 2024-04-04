4889a5ebf15bc53117f683c40fc36dc03bdc189e
Gabriel Lopes <carnagesagas@gmail.com>
2024-04-04 07:48:43 -0300

    teste de UI com texto

--- EstudoPython/Python_UI/MainApp.py
+++ EstudoPython/Python_UI/MainApp.py
@@ -6,4 +6,9 @@ class MainApp(App):
         label = Label(text = 'Fala galera!',
             size_hint = (.5, .5),
             pos_hint = {'center_x': .5, 'center_y': .5})
+        return label
+
+if __name__ ==  '__main__':
+    app = MainApp()
+    app.run()
 
